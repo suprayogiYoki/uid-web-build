c65e9327b162815ab29c13f397369cf9cf00a16d
yoki <yoki@satus-MacBook-Air.local>
2023-09-18 12:02:33 +0700

    integrate api jadwal kelas, dosen pkd on going

---
+++ assets/flutter_service_worker.js
@@ -0,0 +1,306 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"version.json": "191595f72c94967ff44fc676a3aedb8c",
+"index.html": "628cfb847507892c01cc1f6ccd9f72a4",
+"/": "9c5ae611179d45241b673e1afe90451f",
+"main.dart.js": "efd07db9de2c698b76fcbb23b5011ad2",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"editor.html": "a24a2355d4f611a98fe0d047825a0e4a",
+"ckeditor5-build-classic/LICENSE.md": "8782fd9871a0fc1929fa947cbca52026",
+"ckeditor5-build-classic/ckeditor.d.ts": "12baa1ce20d588a5b28919b13d7a62cc",
+"ckeditor5-build-classic/index.html": "9c5ae611179d45241b673e1afe90451f",
+"ckeditor5-build-classic/ckeditor.js": "09e0cc9548435763e40945618292c825",
+"ckeditor5-build-classic/README.md": "5946c79610f1cf0fb232c844d5286a9f",
+"ckeditor5-build-classic/sample/css/sample.css": "dc15e2b44784af190ba96ab845a5bb94",
+"ckeditor5-build-classic/sample/img/github.svg": "729abc9829db40bbc00be0b08302f208",
+"ckeditor5-build-classic/sample/img/umbrellas.jpg": "e935d5c582ff2d43b858681e885c2d95",
+"ckeditor5-build-classic/sample/img/logo.svg": "25510c551d5a9e1d94ac8eb027cffc5f",
+"ckeditor5-build-classic/sample/img/bg.png": "804f8918709610c90a3656f29567108d",
+"ckeditor5-build-classic/ckeditor.js.map": "28adf5a09ad80fb6bfc643919445e7c6",
+"ckeditor5-build-classic/translations/sr-latn.js": "bcd08c8bfebd524056bcb7c4d4e97ce7",
+"ckeditor5-build-classic/translations/pt.js": "966338e5f94d3fa6d8a41b37eaef8b7a",
+"ckeditor5-build-classic/translations/vi.js": "23c35f19ca199c311ac4099901515edf",
+"ckeditor5-build-classic/translations/lv.js": "6ecf25561b1ccfe9f1096048453eceb0",
+"ckeditor5-build-classic/translations/gl.js": "f7ea3d2d136dd6d75b4fd9efc0fc92c6",
+"ckeditor5-build-classic/translations/pl.js": "57cab56803fcd6e6a103505c2c1105f4",
+"ckeditor5-build-classic/translations/el.js": "ac2bde0ce7fb01d8e5b037b219743759",
+"ckeditor5-build-classic/translations/et.js": "804ae24878a9c6f03e8bc6ccbc3be923",
+"ckeditor5-build-classic/translations/sl.js": "e8dba8e2e530b34ed44bf0e91d4b1123",
+"ckeditor5-build-classic/translations/ko.js": "ab413a1cd78b719047a70d3009a00259",
+"ckeditor5-build-classic/translations/hr.js": "1a556242d1ce939e648ffbaeb8575d92",
+"ckeditor5-build-classic/translations/ms.js": "1b9391e8011262ec3dba895213ee8016",
+"ckeditor5-build-classic/translations/fi.js": "e84caef26e81fd7bc728f83c6a5955f0",
+"ckeditor5-build-classic/translations/th.js": "efb2fc125e8c4ae166163b2e42f1154a",
+"ckeditor5-build-classic/translations/jv.js": "27a098507ccdc60e54a71b89c4b501e7",
+"ckeditor5-build-classic/translations/ru.js": "06b9cd935a6a14160b4b3ac22fefe454",
+"ckeditor5-build-classic/translations/eu.js": "6eaf6abe32b7835b086e2fef916a858d",
+"ckeditor5-build-classic/translations/no.js": "787f0ec60786e54e291783c8b4b863c1",
+"ckeditor5-build-classic/translations/kn.js": "a7ae4df2114ce2e5c6a16365df29117c",
+"ckeditor5-build-classic/translations/sq.js": "2281a81fe3f07aea1382b08a29c5a792",
+"ckeditor5-build-classic/translations/gu.js": "914102f9a0faa7a54c2285a3fafcf595",
+"ckeditor5-build-classic/translations/si.js": "63b4bb341a5ad3491b944096789d570f",
+"ckeditor5-build-classic/translations/tt.js": "460e207c900a9c7cca52dde014534ac1",
+"ckeditor5-build-classic/translations/ja.js": "79e2147dc1950956d7a7ac1357489f5d",
+"ckeditor5-build-classic/translations/he.js": "46e608f41ebbe1ebb119b5284d13db3e",
+"ckeditor5-build-classic/translations/ug.js": "2c95c0f96472d1661644ff4e8ffe5c2c",
+"ckeditor5-build-classic/translations/bg.js": "a6a0b63d872976094756e11dd70f58fc",
+"ckeditor5-build-classic/translations/es-co.js": "4292ea5b64cbdd121e2a14c9d2513365",
+"ckeditor5-build-classic/translations/uz.js": "4425962bb66aeb6325cd5f962eed9165",
+"ckeditor5-build-classic/translations/ne.js": "62dda3ffecc649a3d77659554ccd1142",
+"ckeditor5-build-classic/translations/af.js": "3550c398608fe88c3a3fe6795b2f0727",
+"ckeditor5-build-classic/translations/id.js": "6bf7304be734c3de93ed25865d8017f7",
+"ckeditor5-build-classic/translations/az.js": "da34be7082f03562723145ec5c01f054",
+"ckeditor5-build-classic/translations/en-au.js": "f3906acd2f8ad513693b7bb540461143",
+"ckeditor5-build-classic/translations/ca.js": "eef5afad5d7e1037d099a04d9d0d2b4c",
+"ckeditor5-build-classic/translations/nb.js": "11f1498c5554e4930333b515846ae71b",
+"ckeditor5-build-classic/translations/zh-cn.js": "95677a9d408e4e61948f272e56fcbbd9",
+"ckeditor5-build-classic/translations/de-ch.js": "6e2e7cc849728ea2549c7ee2432a7ac8",
+"ckeditor5-build-classic/translations/pt-br.js": "2447e5ff33855479617f364b1ed14e62",
+"ckeditor5-build-classic/translations/oc.js": "0d19da65fee06b7a0e185cd91a5bb279",
+"ckeditor5-build-classic/translations/da.js": "bc61bc4411e8301e8cb700c4025dd3ee",
+"ckeditor5-build-classic/translations/fa.js": "eb5fa01ac4dc35b13ea30b9becc58fa8",
+"ckeditor5-build-classic/translations/de.js": "012c45946c2f6161735228de7564df52",
+"ckeditor5-build-classic/translations/bs.js": "aca0268f9191f2ed1682197466c9cff5",
+"ckeditor5-build-classic/translations/ku.js": "698569f74b8c225965be51191187dc51",
+"ckeditor5-build-classic/translations/tk.js": "fbf8b413901c6670bc7f38f16cac564e",
+"ckeditor5-build-classic/translations/sv.js": "6c01014f4b32f12627627b8896187736",
+"ckeditor5-build-classic/translations/zh.js": "e70a100c82b8d8bfb9df2815f4087e86",
+"ckeditor5-build-classic/translations/hi.js": "4de7036f21f4af1e4be35566bd796f54",
+"ckeditor5-build-classic/translations/uk.js": "74f4498f0d607d9a7e587070924145a4",
+"ckeditor5-build-classic/translations/cs.js": "0005df9b55e12172df72d291eab1f673",
+"ckeditor5-build-classic/translations/km.js": "df7622710978e833fb2f1fa0c638ad7e",
+"ckeditor5-build-classic/translations/fr.js": "dfde7cbc12ce8e9e1900243b4c2c5976",
+"ckeditor5-build-classic/translations/nl.js": "603fce03ab244c972bef1a77dbd7b6f6",
+"ckeditor5-build-classic/translations/en-gb.js": "1c01a87ee3d06ec6f6b2650a9219cf83",
+"ckeditor5-build-classic/translations/sr.js": "90ab5ed6b7b7e7ba9e2bb1353c842bfd",
+"ckeditor5-build-classic/translations/hu.js": "a6fa20479a79ed5b57e79f9a1257e964",
+"ckeditor5-build-classic/translations/lt.js": "498ffcbecd9e8758a5a818670fa0a9ba",
+"ckeditor5-build-classic/translations/ar.js": "41b2ca2b8002ab5fc8a818ae2d92a242",
+"ckeditor5-build-classic/translations/ast.js": "cc638eb66c328566ead187c092a9a96f",
+"ckeditor5-build-classic/translations/sk.js": "7ea29442705327e3dc7eedcc336606cd",
+"ckeditor5-build-classic/translations/it.js": "be10bcea03a862a535c61b17d346ed73",
+"ckeditor5-build-classic/translations/es.js": "8fa0acc3eed61211f716ddcf62ba4934",
+"ckeditor5-build-classic/translations/bn.js": "ca64c59bb5eb18563b387c137361e911",
+"ckeditor5-build-classic/translations/eo.js": "14cc0d52e1af59da290a9d35139d041d",
+"ckeditor5-build-classic/translations/ro.js": "e7c14d7037f694761ce573f78d1e9898",
+"ckeditor5-build-classic/translations/ur.js": "216ec22b34f186f92e5877905912ca00",
+"ckeditor5-build-classic/translations/tr.js": "3013138ce3fb127a81b622e6412306c1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "d37c4da88379fe0a558eae1a785147b2",
+"assets/AssetManifest.json": "5bb7ef25e16dece11fc31fadc2c2f068",
+"assets/NOTICES": "9d7247af16c37923011dabd3d596081f",
+"assets/FontManifest.json": "42bef6d4a8edb8b2efc82c096beb613f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/packages/eva_icons_flutter/lib/fonts/Eva-Icons.ttf": "a71dbd49ad4a52020638190e30ac52e0",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/AssetManifest.bin": "68db9ae49bc48442e718d639317cf728",
+"assets/fonts/MaterialIcons-Regular.otf": "65758eb8b0c3c1b700cd7bca77debf9e",
+"assets/assets/images/default_dosen.png": "f201067c549f6d615d2237ed763dc552",
+"assets/assets/images/raster/man.png": "19a852dd0aec1631c9ca4a67207053a1",
+"assets/assets/uid/images/logo.png": "fe2eca3313b3d2494de949a9e1c6b00b",
+"assets/assets/lang/id.json": "3a58f79334d56fb853196d2234443811",
+"assets/assets/fonts/nunito/Nunito-ExtraBoldItalic.ttf": "5f67429d0ff76ec74d98b915439692ee",
+"assets/assets/fonts/nunito/Nunito-ExtraBold.ttf": "438598eadc302471a495b3133620f621",
+"assets/assets/fonts/nunito/Nunito-ExtraLightItalic.ttf": "ade1717e588935bdec3d38ca19bd1050",
+"assets/assets/fonts/nunito/Nunito-Light.ttf": "08bc5f85e4505782d3fa279944f2feab",
+"assets/assets/fonts/nunito/Nunito-Regular.ttf": "d8de52e6c5df1a987ef6b9126a70cfcc",
+"assets/assets/fonts/nunito/Nunito-SemiBold.ttf": "876701bc4fbf6166f07f152691b15159",
+"assets/assets/fonts/nunito/Nunito-Bold.ttf": "c0844c990ecaaeb9f124758d38df4f3f",
+"assets/assets/fonts/nunito/Nunito-BoldItalic.ttf": "b21199decd37bf9a096e0f495bb20ffd",
+"assets/assets/fonts/nunito/Nunito-Black.ttf": "529509f5501c7f45315210d280a456d4",
+"assets/assets/fonts/nunito/Nunito-ExtraLight.ttf": "4470502486a612e2bf6f38d5bff77ce8",
+"assets/assets/fonts/nunito/Nunito-SemiBoldItalic.ttf": "59cf0995782240777656a355cf08b234",
+"assets/assets/fonts/nunito/Nunito-BlackItalic.ttf": "8bf9ce6e0c5434ca0ac97f90196d0c66",
+"assets/assets/fonts/nunito/Nunito-Italic.ttf": "380d3172fab0055a8d74ab12636f5a9b",
+"assets/assets/fonts/nunito/Nunito-LightItalic.ttf": "55032f4e811253da318f967e0db3d7e9",
+"assets/assets/fonts/Roboto/Roboto-Light.ttf": "881e150ab929e26d1f812c4342c15a7c",
+"assets/assets/fonts/Roboto/Roboto-Regular.ttf": "8a36205bd9b83e03af0591a004bc97f4",
+"assets/assets/fonts/Roboto/Roboto-BoldItalic.ttf": "fd6e9700781c4aaae877999d09db9e09",
+"assets/assets/fonts/Roboto/Roboto-LightItalic.ttf": "5788d5ce921d7a9b4fa0eaa9bf7fec8d",
+"assets/assets/fonts/Roboto/Roboto-Italic.ttf": "cebd892d1acfcc455f5e52d4104f2719",
+"assets/assets/fonts/Roboto/Roboto-BlackItalic.ttf": "c3332e3b8feff748ecb0c6cb75d65eae",
+"assets/assets/fonts/Roboto/Roboto-Bold.ttf": "b8e42971dec8d49207a8c8e2b919a6ac",
+"assets/assets/fonts/Roboto/Roboto-Black.ttf": "d6a6f8878adb0d8e69f9fa2e0b622924",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
